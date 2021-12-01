36fb4cb416da6ec0f7a3d69b8a5a7e86956d26fa
jakob753951 <jlm1999@live.dk>
2021-12-01 16:27:53 +0100

    sum of range instead of manually doing i+1

--- 1-2/1-2.py
+++ 1-2/1-2.py
@@ -1,7 +1,7 @@
 with open('1-1/data.txt') as data_file:
 	points = [int(p) for p in data_file.read().split('\n')]
 
-points_sums = [p+points[i+1]+points[i+2] for i, p in enumerate(points) if i < len(points)-2]
+points_sums = [sum(points[i:i+3]) for i in range(len(points)-2)]
 
 pairs = list(zip(points_sums, points_sums[1:] + points_sums[:1]))[:-1]
 
