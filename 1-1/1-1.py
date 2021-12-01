7c2e748e8ac2ef2215ab8594b16c452055af0e61
jakob753951 <jlm1999@live.dk>
2021-12-01 16:06:39 +0100

    Reverted the solution back to the first attempt

--- 1-1/1-1.py
+++ 1-1/1-1.py
@@ -1,12 +1,8 @@
 with open('1-1/data.txt') as data_file:
-	points = data_file.read().split('\n')
+	points = [int(p) for p in data_file.read().split('\n')]
 
+pairs = list(zip(points, points[1:] + points[:1]))[:-1]
 
-c = 0
-for i in range(len(points)):
-	if i == 0:
-		continue
-	if int(points[i]) > int(points[i-1]):
-		c += 1
+ascending = sum(pair[0] < pair[1] for pair in pairs)
 
-print(c)
+print(ascending)
