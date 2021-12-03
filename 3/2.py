dc2337c80b58cbad3e11dc6274734db8c5de77e9
jakob753951 <jlm1999@live.dk>
2021-12-03 14:54:18 +0100

    fucken finally it works

---
+++ 3/2.py
@@ -0,0 +1,20 @@
+with open('3/data.txt') as data_file:
+	oxygen = co2 = data_file.read().split('\n')
+
+def get_counts(arr):
+	counts = [0] * 12
+	for number in arr:
+		for i, digit in enumerate(number):
+			counts[i] += int(digit)
+	return counts
+
+for i in range(12):
+	if len(oxygen) != 1:
+		oxygen = [num for num in oxygen if int(num[i]) == int(get_counts(oxygen)[i]>=(len(oxygen)/2))]
+	if len(co2) != 1:
+		co2 = [num for num in co2 if int(num[i]) == int(get_counts(co2)[i]<(len(co2)/2))]
+
+oxygen_generator_rating = int(oxygen[0], 2)
+CO2_scrubber_rating = int(co2[0], 2)
+
+print(f'{oxygen_generator_rating} * {CO2_scrubber_rating} = {oxygen_generator_rating * CO2_scrubber_rating}')
