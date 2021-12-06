388e84eb204f5474f55ef77039ca65435b88dec6
jakob753951 <jlm1999@live.dk>
2021-12-06 22:53:09 +0100

    quick fix for day 1 :)

--- 1/1.py
+++ 1/1.py
@@ -1,8 +1,8 @@
 with open('data.txt') as data_file:
 	points = [int(p) for p in data_file.read().split('\n')]
 
-pairs = list(zip(points, points[1:] + points[:1]))[:-1]
+pairs = list(zip(points, points[1:]))
 
-ascending = sum(pair[0] < pair[1] for pair in pairs)
+ascending = [pair[0] < pair[1] for pair in pairs].count(True)
 
 print(ascending)
