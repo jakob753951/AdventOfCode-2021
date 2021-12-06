24a78c90de55d75595cb01d246ca6a9f2020332c
jakob753951 <jlm1999@live.dk>
2021-12-06 22:53:48 +0100

    more day-1 tidying

--- 1/2.py
+++ 1/2.py
@@ -3,8 +3,8 @@ with open('data.txt') as data_file:
 
 points_sums = [sum(points[i:i+3]) for i in range(len(points)-2)]
 
-pairs = list(zip(points_sums, points_sums[1:] + points_sums[:1]))[:-1]
+pairs = list(zip(points_sums, points_sums[1:]))
 
-ascending = sum(pair[0] < pair[1] for pair in pairs)
+ascending = [pair[0] < pair[1] for pair in pairs].count(True)
 
 print(ascending)
