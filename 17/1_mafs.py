01ca76d72025530f1fdd815e69c206e3ec790708
jakob753951 <jlm1999@live.dk>
2021-12-17 21:26:02 +0100

    1_line

--- 17/1_mafs.py
+++ 17/1_mafs.py
@@ -1,3 +1,1 @@
-with open('data.txt') as data_file:
-	y0 = int(data_file.read().split('y=')[1].split('..')[0])
-print(int(((abs(y0)-1)**2+(abs(y0)-1))/2))
+print(int(((abs(int(open('data.txt').read().split('y=')[1].split('..')[0]))-1)**2+(abs(int(open('data.txt').read().split('y=')[1].split('..')[0]))-1))/2))
