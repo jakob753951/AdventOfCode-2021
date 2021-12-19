217288c423bf92ad10986a929b60c74d4dd37580
Jakob Ladegaard Møller <jlm1999@live.dk>
2021-12-19 15:10:50 +0100

    updated 7 to use better maths

--- 7/2.py
+++ 7/2.py
@@ -1,6 +1,10 @@
-with open('data.txt') as data_file:
+with open('7/data.txt') as data_file:
 	crabs = [int(p) for p in data_file.read().split(',')]
 
-costs = [sum(sum(range(abs(i-crab)+1)) for crab in crabs) for i in range(min(crabs), max(crabs)+1)]
+
+def f(x):
+	return int((x**2+x)/2)
+
+costs = [sum(f(abs(i-crab)) for crab in crabs) for i in range(min(crabs), max(crabs)+1)]
 
 print(min(costs))
